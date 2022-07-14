78ae651e63eab8f10953c70ee1de01adade91ef8
scardenasb <84429399+scardenasb@users.noreply.github.com>
2022-07-14 04:37:00 -0400

    refac(TODO): clean TODO's

--- main.py
+++ main.py
@@ -2,7 +2,4 @@
 
 import os
 
-
-# TODO: Connect a sql base to keep track of this data
-# TODO: Schedule an autoexe to run this scrip once a day (heroku add on)
 os.system("python3 ./src/connector.py")
